e6f7defd979e78544643c068a75d5427d62e0d07
kiemt <kiemt@QUYNH>
2022-03-10 23:19:30 +0700

    Add project files.

---
+++ QLDSV_HTC/frmDangNhap.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace QLDSV_HTC
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.cmbKhoa = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDN = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.chkShowPass = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KHOA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MẬT KHẨU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "TÀI KHOẢN";
+            // 
+            // txtTK
+            // 
+            this.txtTK.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTK.Location = new System.Drawing.Point(221, 141);
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(270, 27);
+            this.txtTK.TabIndex = 3;
+            // 
+            // txtMK
+            // 
+            this.txtMK.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.Location = new System.Drawing.Point(221, 191);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(270, 27);
+            this.txtMK.TabIndex = 4;
+            this.txtMK.Text = "123";
+            this.txtMK.UseSystemPasswordChar = true;
+            // 
+            // cmbKhoa
+            // 
+            this.cmbKhoa.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbKhoa.FormattingEnabled = true;
+            this.cmbKhoa.Location = new System.Drawing.Point(226, 65);
+            this.cmbKhoa.Name = "cmbKhoa";
+            this.cmbKhoa.Size = new System.Drawing.Size(270, 27);
+            this.cmbKhoa.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(562, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "QUẢN LÝ ĐIỂM SINH VIÊN HTC";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnDN
+            // 
+            this.btnDN.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDN.Location = new System.Drawing.Point(66, 267);
+            this.btnDN.Name = "btnDN";
+            this.btnDN.Size = new System.Drawing.Size(166, 40);
+            this.btnDN.TabIndex = 7;
+            this.btnDN.Text = "ĐĂNG NHẬP";
+            this.btnDN.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(325, 267);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(166, 40);
+            this.btnThoat.TabIndex = 8;
+            this.btnThoat.Text = "THOÁT";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // chkShowPass
+            // 
+            this.chkShowPass.AutoSize = true;
+            this.chkShowPass.BackColor = System.Drawing.SystemColors.Control;
+            this.chkShowPass.Font = new System.Drawing.Font("Times New Roman", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkShowPass.Location = new System.Drawing.Point(352, 220);
+            this.chkShowPass.Name = "chkShowPass";
+            this.chkShowPass.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.chkShowPass.Size = new System.Drawing.Size(159, 24);
+            this.chkShowPass.TabIndex = 9;
+            this.chkShowPass.Text = "Hiển thị mật khẩu";
+            this.chkShowPass.UseVisualStyleBackColor = false;
+            this.chkShowPass.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 341);
+            this.Controls.Add(this.chkShowPass);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbKhoa);
+            this.Controls.Add(this.txtMK);
+            this.Controls.Add(this.txtTK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDangNhap";
+            this.Text = "frmDangNhap";
+            this.Load += new System.EventHandler(this.frmDangNhap_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTK;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.ComboBox cmbKhoa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDN;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.CheckBox chkShowPass;
+    }
+}
---
+++ QLDSV_HTC/frmMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLDSV_HTC
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            frmDangNhap f = new frmDangNhap();
+            f.Show();
+        }
+    }
+}
